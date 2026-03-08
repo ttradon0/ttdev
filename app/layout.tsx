8ae21d1b808067b145c522d8b12f0d94b5381542
ttradon0 <ttradon0@gmail.com>
2026-03-08 14:21:31 +0700

    Change font to Prompt from Google Fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Prompt } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const prompt = Prompt({
+  weight: "400",
+  variable: "--font-prompt",
   subsets: ["latin"],
 });
 
@@ -24,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${prompt.variable} antialiased`}>
         {children}
       </body>
     </html>
